05b7145f0b4d02272a74646d72bf766e5bea429c
meppydc <49413486+meppydc@users.noreply.github.com>
2020-08-10 17:38:50 -0400

    token from config

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
 
+import config
+
 client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready(): 
     print('Bot is ready.')
 
-client.run('test')
+client.run(config.TOKEN)
