0a72f5de0a8983f1958c320e2eef56b0b71d1187
Idosho <ido.shoshani1@gmail.com>
2020-08-10 17:49:13 -0400

    ping

--- bot.py
+++ bot.py
@@ -10,3 +10,8 @@ async def on_ready():
     print('Bot is ready.')
 
 client.run(config.TOKEN)
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
