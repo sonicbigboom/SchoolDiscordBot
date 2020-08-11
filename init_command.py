23fed577bddcb67704cbd7c5293dc9b14392f1e4
meppydc <49413486+meppydc@users.noreply.github.com>
2020-08-11 17:20:02 -0400

    init command now uses is_admin decorator

--- init_command.py
+++ init_command.py
@@ -1,31 +1,25 @@
 import client
 
 @client.client.command()
+@client.checks.is_admin()
 async def init(ctx, *, arg):
     message = ctx.message
-    author = message.author
-    guild_permissions = author.guild_permissions
+    author = ctx.author
 
-    if guild_permissions.administrator:
-        guild = message.guild
-        ref = client.db.reference(f'{guild.id}')
+    guild = message.guild
+    ref = client.db.reference(f'{guild.id}')
 
-        if ref.get() is None:
-            ref = client.db.reference('/')
-            ref.set({
-                f'{guild.id}': 
-                    {
-                        'Name': f'{arg}',
-                        'Classes': '{}'
-                    }   
-            })
-            await ctx.send(f'>>> Adding new school {arg} with server ID: {guild.id}')
-        else:
-            await ctx.send(f'>>> School with this server id already exists!')
+    if ref.get() is None:
+        ref = client.db.reference('/')
+        ref.set({
+            f'{guild.id}': 
+                {
+                    'Name': f'{arg}',
+                    'Classes': '{}'
+                }   
+        })
+        await ctx.send(f'>>> Adding new school {arg} with server ID: {guild.id}')
     else:
-        await ctx.send(f'>>> You must have Administrator permissions to use this command.')
+        await ctx.send(f'>>> School with this server id already exists!')
 
-
-        
-    
-    
+  
