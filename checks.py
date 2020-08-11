b42236a489e137c46f35451de7d7912157c59311
Idosho <ido.shoshani1@gmail.com>
2020-08-11 17:23:22 -0400

    fix

--- checks.py
+++ checks.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 def is_admin():
     async def predicate(ctx):
-        if ctx.author.guild_permissions == guild_permissions.administrator:
+        if ctx.author.guild_permissions.administrator:
             return True
         else:
             await ctx.send('>>> You must have Administrator permissions to use this command.')
