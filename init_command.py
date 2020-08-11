a7baa2ca0bb9a2c3b484373ed24692a782696c0d
sonicbigboom <sbb-gitlab@dsgt.com>
2020-08-11 10:04:43 -0400

    Minor command improvements.

--- init_command.py
+++ init_command.py
@@ -1,21 +1,30 @@
 import client
 
 @client.client.command()
-async def init(ctx, arg):
-    guild = ctx.message.guild
-    ref = client.db.reference(f'{guild.id}')
+async def init(ctx, *, arg):
+    message = ctx.message
+    author = message.author
+    guild_permissions = author.guild_permissions
 
-    if ref.get() is None:
-        ref = client.db.reference('/')
-        ref.set({
-            f'{guild.id}': 
-                {
-                    'Name': f'{arg}'
-                }   
-        })
-        await ctx.send(f'```Adding new school {arg} with server ID: {guild.id}```')
+    if guild_permissions.administrator:
+        guild = message.guild
+        ref = client.db.reference(f'{guild.id}')
+
+        if ref.get() is None:
+            ref = client.db.reference('/')
+            ref.set({
+                f'{guild.id}': 
+                    {
+                        'Name': f'{arg}'
+                    }   
+            })
+            await ctx.send(f'>>> Adding new school {arg} with server ID: {guild.id}')
+        else:
+            await ctx.send(f'>>> School with this server id already exists!')
     else:
-        await ctx.send(f'```School with this server id already exists!```')
+        await ctx.send(f'>>> You must have Administrator permissions to use this command.')
+
+
         
     
     
--- ping_command.py
+++ ping_command.py
@@ -2,10 +2,11 @@ import client
 
 @client.client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.client.latency * 1000)}ms')
+    await ctx.send(f'>>> Pong! {round(client.client.latency * 1000)}ms')
 
-
+"""
 @client.client.command()
 async def create(ctx):
     guild = ctx.message.guild
     await guild.create_voice_channel('cool-channel')
+"""
