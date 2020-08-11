32b8868e68988e01a6ee53c9c03e62980eab23eb
Idosho <ido.shoshani1@gmail.com>
2020-08-10 22:33:04 -0400

    init command

--- init_command.py
+++ init_command.py
@@ -3,4 +3,19 @@ import client
 @client.client.command()
 async def init(ctx, arg):
     guild = ctx.message.guild
-    await ctx.send(f'Adding new school with server ID: {guild.id}')
+    ref = client.db.reference(f'{guild.id}')
+
+    if ref.get() is None:
+        ref = client.db.reference('/')
+        ref.set({
+            f'{guild.id}': 
+                {
+                    'Name': f'{arg}'
+                }   
+        })
+        await ctx.send(f'```Adding new school {arg} with server ID: {guild.id}```')
+    else:
+        await ctx.send(f'```School with this server id already exists!```')
+        
+    
+    
