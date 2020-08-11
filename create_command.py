ed7244729cd19f59b37a85586df54b4c5d2f3f76
meppydc <49413486+meppydc@users.noreply.github.com>
2020-08-11 18:47:32 -0400

    add admin check

--- create_command.py
+++ create_command.py
@@ -1,19 +1,17 @@
 import client
 
 @client.client.command()
+@client.checks.is_admin()
 async def create(ctx, *, arg):
     message = ctx.message
-    author = message.author
-    guild_permissions = author.guild_permissions
+    author = ctx.author
+    
+    guild = ctx.guild
+    
+    ref = client.db.reference(f'{guild.id}/Classes')
+    class_ref = ref.push({
+        'Class Name': f'{arg}',
+    })    
 
-    if guild_permissions.administrator:
-        guild = message.guild
-        
-        ref = client.db.reference(f'{guild.id}/Classes')
-        class_ref = ref.push({
-            'Class Name': f'{arg}',
-        })    
-
-        await ctx.send(f'>>> New class "{arg}" was added with class code: "{class_ref.key}"')
-    else:
-        await ctx.send(f'>>> You must have Administrator permissions to use this command.')
+    await ctx.send(f'>>> New class "{arg}" was added with class code: "{class_ref.key}"')
+    
