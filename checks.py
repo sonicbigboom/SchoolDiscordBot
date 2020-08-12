56ce588e3fef44f26ad3b3431fc1cb8ed802d94a
meppydc <49413486+meppydc@users.noreply.github.com>
2020-08-12 17:12:36 -0400

    actually made naive arg check work

--- checks.py
+++ checks.py
@@ -33,8 +33,11 @@ def is_admin():
 
 def naive_arg_check(num_args):
     async def predicate(ctx):
-        args = ctx.message.split(' ')[1:]
+        args = len(ctx.message.content.split(' ')[1:])
         print(f'Passed in {args} arguments and command takes in {num_args}')
-        return args == num_args
+        if args == num_args:
+            return True
+        else:
+            await ctx.send(f'>>> This command requires {num_args} arguments and you passed in {args}.')
     
     return commands.check(predicate)
